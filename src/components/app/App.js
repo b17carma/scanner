277e288d456318bae715c3b7f6e7d6664f7c73a6
Carl Maier <b17carma@student.his.se>
2020-02-19 13:13:21 +0100

    Add material ui

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppHeader from "../appbar/AppHeader";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+function App() {
+    return (
+        <div className="App">
+            <CssBaseline/>
+            <AppHeader/>
+        </div>
+    );
+}
+
+export default App;
