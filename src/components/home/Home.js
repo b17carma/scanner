5aac812e19a1f74acb2d70cd61aa081b8acae2a1
b17carma <b17carma@student.his.se>
2020-02-20 21:39:10 +0100

    Add test implementation of qr scanner

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import QrScanner from "../qrreader/QrScanner";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -17,13 +18,7 @@ export default function Home() {
     return (
         <Container>
             <Paper className={classes.root}>
-                <p>Hello</p>
-            </Paper>
-            <Paper className={classes.root}>
-                <p>Hello</p>
-            </Paper>
-            <Paper className={classes.root}>
-                <p>Hello</p>
+                <QrScanner/>
             </Paper>
         </Container>
     );
---
+++ src/components/qrreader/QrScanner.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import QrReader from "react-qr-reader";
+
+export default function QrScanner() {
+
+    const [value, setValue] = useState("No Result");
+
+    const handleScan = data => {
+        if (data) {
+            setValue(data)
+        }
+    };
+
+    const previewStyle = {
+    };
+
+    const handleError = err => {
+        console.error(err);
+    };
+
+    return (
+        <div>
+            <QrReader
+                delay={300}
+                onError={handleError}
+                onScan={handleScan}
+                style={previewStyle}
+            />
+            <p>Value: {value}</p>
+        </div>
+    )
+}
