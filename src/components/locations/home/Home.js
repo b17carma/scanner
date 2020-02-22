06fc7c36519b96b8be8a6baef12b2b56808d2081
b17carma <b17carma@student.his.se>
2020-02-22 22:25:30 +0100

    Update .gitignore & remove unnecessary imports

--- src/components/locations/home/Home.js
+++ src/components/locations/home/Home.js
@@ -1,15 +1,10 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import {Link} from "react-router-dom";
 import List from "@material-ui/core/List";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItem from "@material-ui/core/ListItem";
 import {Send as SendIcon} from "@material-ui/icons"
 import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles(theme => ({
     root: {
