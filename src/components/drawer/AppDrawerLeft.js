820f8e8b0f62df20f409232c2cf3c9c45cd10bd7
b17carma <b17carma@student.his.se>
2020-02-23 16:36:46 +0100

    Further improve navigation

--- src/components/drawer/AppDrawerLeft.js
+++ src/components/drawer/AppDrawerLeft.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import routes from "./Routes"
-import {withRouter} from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const styles = () => ({
@@ -22,43 +20,39 @@ class AppDrawerLeft extends React.Component {
         return this.props.location.pathname === routeName;
     }
 
-    navList(props) {
-        const { classes } = props;
-
-        return (
-            <div
-                className={classes.list}
-                role="presentation"
-                onClick={this.props.toggleDrawer(false)}
-                onKeyDown={this.props.toggleDrawer(false)}
-            >
-                <List>
-                    <ListItem>
-                        <ListItemText primary="Equipment Scanner"/>
-                    </ListItem>
-
-                    {routes.map((prop, key) => {
-                        return (
-                            <ListItem selected={this.currentRoute(prop.path)} button key={prop.sideBarName} component={Link}
-                                      to={prop.path}>
-                                <ListItemIcon>
-                                    <prop.icon/>
-                                </ListItemIcon>
-                                <ListItemText primary={prop.sideBarName}/>
-                            </ListItem>
-                        )
-                    })}
-                </List>
-            </div>
-        )
-    }
-
     render() {
+        const {classes} = this.props;
+
         return (
             <Drawer open={this.props.open} onClose={this.props.toggleDrawer(false)}>
-                {this.navList(this.props)}
+                <div
+                    className={classes.list}
+                    role="presentation"
+                    onClick={this.props.toggleDrawer(false)}
+                    onKeyDown={this.props.toggleDrawer(false)}
+                >
+                    <List>
+                        <ListItem>
+                            <ListItemText primary="Equipment Scanner"/>
+                        </ListItem>
+
+                        {routes.map((prop, key) => {
+                            return (
+                                <ListItem selected={this.currentRoute(prop.path)} button key={prop.sideBarName}
+                                          component={Link}
+                                          to={prop.path}>
+                                    <ListItemIcon>
+                                        <prop.icon/>
+                                    </ListItemIcon>
+                                    <ListItemText primary={prop.sideBarName}/>
+                                </ListItem>
+                            )
+                        })}
+                    </List>
+                </div>
             </Drawer>
         )
     }
 }
+
 export default withStyles(styles)(withRouter(AppDrawerLeft));
