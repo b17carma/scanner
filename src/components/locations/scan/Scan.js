4976412c077d881490c53fa993bc1138750d97cd
b17carma <b17carma@student.his.se>
2020-02-23 17:38:28 +0100

    Display skeleton while loading equipment information

--- src/components/locations/scan/Scan.js
+++ src/components/locations/scan/Scan.js
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import Paper from "@material-ui/core/Paper";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Container from "@material-ui/core/Container";
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -12,12 +13,14 @@ const useStyles = makeStyles(theme => ({
 export default function Scan(props) {
 
     const [parts, setParts] = React.useState({});
+    const [loading, setLoading] = React.useState(true);
 
     useEffect(() => {
         async function fetchData() {
             const res = await fetch("https://jsonplaceholder.typicode.com/todos/" + props.match.params.id);
             const data = await res.json();
             setParts(data);
+            setLoading(false);
         }
 
         fetchData();
@@ -28,9 +31,13 @@ export default function Scan(props) {
 
     return (
         <Container>
-            <Paper className={classes.root}>
-                {parts.title}
-            </Paper>
+            {loading ? [...new Array(4)].map((item, index) => (
+                <Skeleton variant="text"/>
+            )) : (
+                <Paper className={classes.root}>
+                    {parts.title}
+                </Paper>
+            )}
         </Container>
     )
 }
