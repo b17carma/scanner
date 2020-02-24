324ccaea2a59c775122fc38ac2952f6e69379d62
Carl Maier <b17carma@student.his.se>
2020-02-24 13:26:38 +0100

    Use recharts instead of frappe charts

--- src/components/locations/analytics/Analytics.js
+++ src/components/locations/analytics/Analytics.js
@@ -5,6 +5,9 @@ import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import {ExpandMore as ExpandMoreIcon} from "@material-ui/icons"
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import {
+    PieChart, Pie, Sector, Cell,
+} from 'recharts';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -14,6 +17,26 @@ const useStyles = makeStyles(theme => ({
         fontWeight: theme.typography.fontWeightRegular,
     },
 }));
+
+const data01 = [
+    { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
+];
+
+
+const data02 = [
+    { name: 'A1', value: 100 },
+    { name: 'A2', value: 300 },
+    { name: 'B1', value: 100 },
+    { name: 'B2', value: 80 },
+    { name: 'B3', value: 40 },
+    { name: 'B4', value: 30 },
+    { name: 'B5', value: 50 },
+    { name: 'C1', value: 100 },
+    { name: 'C2', value: 200 },
+    { name: 'D1', value: 150 },
+    { name: 'D2', value: 50 },
+];
 
 export default function Analytics() {
     const classes = useStyles();
@@ -26,13 +49,13 @@ export default function Analytics() {
                         aria-controls="panel1a-content"
                         id="panel1a-header"
                     >
-                        <Typography className={classes.heading}>Expansion Panel 1</Typography>
+                        <Typography className={classes.heading}>24/02/2020</Typography>
                     </ExpansionPanelSummary>
                     <ExpansionPanelDetails>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
-                        </Typography>
+                        <PieChart width={400} height={400}>
+                            <Pie data={data01} dataKey="value" cx={200} cy={200} outerRadius={60} fill="#8884d8" />
+                            <Pie data={data02} dataKey="value" cx={200} cy={200} innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                        </PieChart>
                     </ExpansionPanelDetails>
                 </ExpansionPanel>
                 <ExpansionPanel>
@@ -41,7 +64,7 @@ export default function Analytics() {
                         aria-controls="panel1a-content"
                         id="panel1a-header"
                     >
-                        <Typography className={classes.heading}>Expansion Panel 1</Typography>
+                        <Typography className={classes.heading}>23/02/2020</Typography>
                     </ExpansionPanelSummary>
                     <ExpansionPanelDetails>
                         <Typography>
