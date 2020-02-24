3e3cd307d514e3ba62737fd70df870d331411681
Carl Maier <b17carma@student.his.se>
2020-02-24 13:30:56 +0100

    Add sample chart

--- src/components/locations/analytics/Analytics.js
+++ src/components/locations/analytics/Analytics.js
@@ -6,73 +6,61 @@ import Typography from "@material-ui/core/Typography";
 import {ExpandMore as ExpandMoreIcon} from "@material-ui/icons"
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {
-    PieChart, Pie, Sector, Cell,
+    PieChart, Pie, Sector, Cell, AreaChart,
 } from 'recharts';
+import ResponsiveContainer from "recharts/lib/component/ResponsiveContainer";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-    },
+    root: {},
     heading: {
         fontSize: theme.typography.pxToRem(15),
         fontWeight: theme.typography.fontWeightRegular,
     },
 }));
 
-const data01 = [
-    { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
-];
-
-
-const data02 = [
-    { name: 'A1', value: 100 },
-    { name: 'A2', value: 300 },
-    { name: 'B1', value: 100 },
-    { name: 'B2', value: 80 },
-    { name: 'B3', value: 40 },
-    { name: 'B4', value: 30 },
-    { name: 'B5', value: 50 },
-    { name: 'C1', value: 100 },
-    { name: 'C2', value: 200 },
-    { name: 'D1', value: 150 },
-    { name: 'D2', value: 50 },
+const data = [
+    {name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+    {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
 ];
 
 export default function Analytics() {
     const classes = useStyles();
 
     return (
-            <div className={classes.root}>
-                <ExpansionPanel>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography className={classes.heading}>24/02/2020</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                        <PieChart width={400} height={400}>
-                            <Pie data={data01} dataKey="value" cx={200} cy={200} outerRadius={60} fill="#8884d8" />
-                            <Pie data={data02} dataKey="value" cx={200} cy={200} innerRadius={70} outerRadius={90} fill="#82ca9d" label />
-                        </PieChart>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-                <ExpansionPanel>
-                    <ExpansionPanelSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography className={classes.heading}>23/02/2020</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
-                        <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                            sit amet blandit leo lobortis eget.
-                        </Typography>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
-            </div>
+        <div className={classes.root}>
+            <ExpansionPanel>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography className={classes.heading}>24/02/2020</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <div style={{width: '100%', height: 300}}>
+                        <ResponsiveContainer>
+                            <PieChart>
+                                <Pie dataKey="value" data={data} fill="#8884d8" label/>
+                            </PieChart>
+                        </ResponsiveContainer>
+                    </div>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography className={classes.heading}>23/02/2020</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+        </div>
     );
 }
