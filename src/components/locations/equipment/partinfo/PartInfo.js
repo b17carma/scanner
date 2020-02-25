5180396e7dbf8ff115d059fae3df12c0bd7f5c9e
b17carma <b17carma@student.his.se>
2020-02-26 00:36:46 +0100

    Fix broken link

--- src/components/locations/equipment/partinfo/PartInfo.js
+++ src/components/locations/equipment/partinfo/PartInfo.js
@@ -2,7 +2,6 @@ import React, {useEffect} from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Typography from "@material-ui/core/Typography";
 import CardContent from "@material-ui/core/CardContent";
-import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import {Container} from "@material-ui/core";
 import List from "@material-ui/core/List";
