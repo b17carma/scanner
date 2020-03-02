d011bed944492b372dd74167e15defaff6b87ae8
Carl Maier <b17carma@student.his.se>
2020-03-02 14:10:36 +0100

    Improve scan result layout

--- src/components/locations/scan/ScanResult.js
+++ src/components/locations/scan/ScanResult.js
@@ -9,6 +9,7 @@ import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
 
 export default function ScanResult(props) {
 
@@ -17,11 +18,12 @@ export default function ScanResult(props) {
     const useStyles = makeStyles(theme => ({
         root: {
             padding: theme.spacing(1),
-            height: "100%",
-            backgroundColor: theme.palette.background.paper
         },
         media: {
             height: 150
+        },
+        paper: {
+            marginBottom: theme.spacing(1)
         },
         button: {
             marginRight: theme.spacing(1)
@@ -45,13 +47,15 @@ export default function ScanResult(props) {
         })
     }
 
-    const [part, setPart] = React.useState([]);
+    const [part, setPart] = React.useState({});
+    const [loading, setLoading] = React.useState(true);
 
     useEffect(() => {
         async function fetchData() {
             const res = await fetch(process.env.REACT_APP_API_LOCATION + "/parts/" + props.match.params.equipmentId + "/" + props.match.params.partId);
             const data = await res.json();
             setPart(data);
+            setLoading(false);
         }
 
         fetchData();
@@ -59,6 +63,11 @@ export default function ScanResult(props) {
     }, [props.match.params.equipmentId, props.match.params.partId]);
 
     const classes = useStyles();
+
+    if (loading)
+        return (
+            <div/>
+        );
 
     return (
         <Box className={classes.root}>
@@ -70,20 +79,22 @@ export default function ScanResult(props) {
                 />
                 <CardContent>
                     <Typography variant="h5" component="h2">
-                        {part.identifier}
+                        {part.equipment.identifier} - {part.identifier}
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
+                    <Typography gutterBottom variant="body2" color="textSecondary" component="p">
                         {part.description}
                     </Typography>
                 </CardContent>
             </Card>
-            <Card>
-                <Typography variant="h5">
-                    Is this component functioning properly?
-                </Typography>
-                <Button className={classes.button} variant="contained" onClick={() => sendResults(true)}>Yes</Button>
-                <Button className={classes.button} variant="contained" onClick={() => sendResults(false)}>No</Button>
-                <Button className={classes.button} variant="contained" onClick={() => history.push("/")}>Cancel</Button>
+            <Card className={classes.paper} variant="outlined">
+                <CardContent>
+                    <Typography variant="body1" component="p" gutterBottom>
+                        Is this component functioning properly?
+                    </Typography>
+                    <Button variant="outlined" color="primary" className={classes.button} onClick={() => sendResults(true)}>Yes</Button>
+                    <Button variant="outlined" color="primary" className={classes.button} onClick={() => sendResults(false)}>No</Button>
+                    <Button variant="outlined" color="primary" className={classes.button} onClick={() => history.push("/")}>Cancel</Button>
+                </CardContent>
             </Card>
         </Box>
     )
