c48fc79ec331981ff17777b621cae1570ef7eb42
b17carma <b17carma@student.his.se>
2020-03-09 16:15:22 +0100

    Separate home overview list

---
+++ src/components/locations/home/HomeOverviewList.js
@@ -0,0 +1,73 @@
+import ListSubheader from "@material-ui/core/ListSubheader";
+import ListItem from "@material-ui/core/ListItem";
+import {Link} from "react-router-dom";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import HistoryIcon from "@material-ui/icons/History";
+import CheckIcon from "@material-ui/icons/Check";
+import {green, red} from "@material-ui/core/colors";
+import WarningIcon from "@material-ui/icons/Warning";
+import ListItemText from "@material-ui/core/ListItemText";
+import React, {useEffect, useState} from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(theme => ({
+    alert: {
+        marginBottom: theme.spacing(1)
+    },
+    list: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    listSection: {
+        backgroundColor: 'inherit',
+    },
+    ul: {
+        backgroundColor: 'inherit',
+        padding: 0,
+    }
+}));
+
+export default function HomeOverviewList() {
+    const classes = useStyles();
+
+    const [overview, setOverview] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchOverviewData() {
+            const res = await fetch(process.env.REACT_APP_API_LOCATION + "/analytics/overview");
+
+            res.json().then((data) => {
+                setOverview(data);
+                setLoading(false)
+            }).catch(console.log)
+        }
+
+        fetchOverviewData();
+
+        console.log("Fetched overview data");
+    }, []);
+
+    if (loading) {
+        return (
+            <div/>
+        );
+    }
+
+    return (
+        overview.map((intervalData, index) => (
+            <li key={`overview-${index}`} className={classes.listSection}>
+                <ul className={classes.ul}>
+                    <ListSubheader>{intervalData.date}</ListSubheader>
+                    {intervalData.data.map((component, i) => (
+                        <ListItem button key={`item-${i}`} component={Link} to={"/equipment/" + component.equipment._id + "/" + component._id}>
+                            <ListItemIcon>
+                                {!component.hasOwnProperty('scanStatus') ? <HistoryIcon/> : component.scanStatus ? <CheckIcon style={{color: green[500]}}/> : <WarningIcon style={{color: red[500]}}/>}
+                            </ListItemIcon>
+                            <ListItemText primary={`${component.equipment.identifier} - ${component.identifier}`}/>
+                        </ListItem>
+                    ))}
+                </ul>
+            </li>
+        ))
+    )
+}
--- src/components/locations/home/HomeView.js
+++ src/components/locations/home/HomeView.js
@@ -1,91 +1,26 @@
-import React, {useEffect, useState} from "react";
-import Container from "@material-ui/core/Container";
+import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import CheckIcon from "@material-ui/icons/Check";
-import {green, red} from "@material-ui/core/colors";
-import WarningIcon from "@material-ui/icons/Warning";
 import List from "@material-ui/core/List";
 import Box from "@material-ui/core/Box";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import HistoryIcon from "@material-ui/icons/History";
-import {Link} from "react-router-dom";
+import HomeOverviewList from "./HomeOverviewList";
 
 const useStyles = makeStyles(theme => ({
     root: {
         marginBottom: theme.spacing(8)
     },
-    paper: {
-        padding: theme.spacing(1),
-        marginBottom: theme.spacing(1),
-    },
-    alert: {
-        marginBottom: theme.spacing(1)
-    },
     list: {
         backgroundColor: theme.palette.background.paper,
     },
-    listSection: {
-        backgroundColor: 'inherit',
-    },
-    ul: {
-        backgroundColor: 'inherit',
-        padding: 0,
-    }
 }));
 
 export default function HomeView() {
     const classes = useStyles();
 
-    const [overview, setOverview] = useState([]);
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function fetchStepData() {
-            const res = await fetch(process.env.REACT_APP_API_LOCATION + "/analytics/overview");
-
-            res.json().then((data) => {
-                setOverview(data);
-                setLoading(false)
-            }).catch(console.log)
-        }
-
-        fetchStepData();
-
-        console.log("Fetched step data");
-    }, []);
-
-    function ListItems() {
-        return (
-            overview.map((overviewItem, index) => (
-                <li key={`overview-${index}`} className={classes.listSection}>
-                    <ul className={classes.ul}>
-                        <ListSubheader>{overviewItem.date}</ListSubheader>
-                        {overviewItem.data.map((item, i) => (
-                            <ListItem button key={`item-${i}`} component={Link} to={"/equipment/" + item.equipment._id + "/" + item._id}>
-                                <ListItemIcon>
-                                    {!item.hasOwnProperty('scanStatus') ? <HistoryIcon/> : item.scanStatus ? <CheckIcon style={{color: green[500]}}/> : <WarningIcon style={{color: red[500]}}/>}
-                                </ListItemIcon>
-                                <ListItemText primary={`${item.equipment.identifier} - ${item.identifier}`}/>
-                            </ListItem>
-                        ))}
-                    </ul>
-                </li>
-            ))
-        )
-    }
-
-    if (loading) {
-        return (
-            <div/>
-        );
-    }
     return (
         <Box className={classes.root}>
             <List className={classes.list}>
-                <ListItems/>
+                <HomeOverviewList/>
             </List>
         </Box>
     )
