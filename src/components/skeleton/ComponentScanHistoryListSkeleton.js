a330c51c77b391a09eb8d27a2aa0a647786a4055
b17carma <b17carma@student.his.se>
2020-03-09 21:09:58 +0100

    Remove overline text from scan history list skeleton

--- src/components/skeleton/ComponentScanHistoryListSkeleton.js
+++ src/components/skeleton/ComponentScanHistoryListSkeleton.js
@@ -3,23 +3,19 @@ import Skeleton from "@material-ui/lab/Skeleton";
 import React from "react";
 import List from "@material-ui/core/List";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ContainedOverlineText from "../util/ContainedOverlineText";
 
 export default function ComponentListItemsSkeleton(props) {
     return (
-        <div>
-            <ContainedOverlineText text="Recent scans"/>
-            <List className={props.classes.list}>
-                {[...new Array(5)].map((item, index) => (
-                        <ListItem key={index} button>
-                            <ListItemIcon>
-                                <Skeleton variant="circle" width={30} height={30}/>
-                            </ListItemIcon>
-                            <Skeleton variant="text" width={150}/>
-                        </ListItem>
-                    )
-                )}
-            </List>
-        </div>
+        <List className={props.classes.list}>
+            {[...new Array(5)].map((item, index) => (
+                    <ListItem key={index} button>
+                        <ListItemIcon>
+                            <Skeleton variant="circle" width={30} height={30}/>
+                        </ListItemIcon>
+                        <Skeleton variant="text" width={150}/>
+                    </ListItem>
+                )
+            )}
+        </List>
     )
 }
