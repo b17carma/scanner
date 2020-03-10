26a8b6d3a10ab378c2e09e4a9f94c04520df6e50
b17carma <b17carma@student.his.se>
2020-03-10 23:54:39 +0100

    Modify scan now appearance

--- src/components/locations/equipment/componentlist/componentinformation/ComponentActions.js
+++ src/components/locations/equipment/componentlist/componentinformation/ComponentActions.js
@@ -1,16 +1,15 @@
-import Card from "@material-ui/core/Card";
-import Button from "@material-ui/core/Button";
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {Link} from "react-router-dom";
-import {CardActions} from "@material-ui/core";
+import ListItem from "@material-ui/core/ListItem";
+import List from "@material-ui/core/List";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import {Scanner as ScannerIcon} from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
-    paper: {
-        marginBottom: theme.spacing(1)
-    },
-    button: {
-        marginRight: theme.spacing(1)
+    list: {
+        background: theme.palette.background.paper
     }
 }));
 
@@ -18,10 +17,13 @@ export default function ComponentActions() {
     const classes = useStyles();
 
     return (
-        <Card className={classes.paper} elevation={0}>
-            <CardActions>
-                <Button variant="outlined" color="primary" className={classes.button} component={Link} to="/scan">Scan Now</Button>
-            </CardActions>
-        </Card>
+        <List className={classes.list}>
+            <ListItem button component={Link} to="/scan">
+                <ListItemIcon>
+                    <ScannerIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Scan Now"/>
+            </ListItem>
+        </List>
     )
 }
