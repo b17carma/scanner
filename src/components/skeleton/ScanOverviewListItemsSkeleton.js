494e5f8c29b202f2aa0da25454cfcea5d8f1ac89
b17carma <b17carma@student.his.se>
2020-03-26 17:48:58 +0100

    Update packages & refactor ScanOverviewListItemsSkeleton.js

--- src/components/skeleton/ScanOverviewListItemsSkeleton.js
+++ src/components/skeleton/ScanOverviewListItemsSkeleton.js
@@ -4,8 +4,12 @@ import ListItem from "@material-ui/core/ListItem";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import List from "@material-ui/core/List";
 
 const useStyles = makeStyles(theme => ({
+    list: {
+        backgroundColor: theme.palette.background.paper
+    },
     listSection: {
         backgroundColor: 'inherit',
     },
@@ -19,23 +23,26 @@ export default function ScanOverviewListItemsSkeleton() {
     const classes = useStyles();
 
     return (
-        [...new Array(5)].map((item, index) => (
-            <li key={index} className={classes.listSection}>
-                <ul className={classes.ul}>
-                    <ListSubheader>
-                        <Skeleton variant="text" width={70} height={20}/>
-                    </ListSubheader>
-                    {[...new Array(4)].map((item, i) => (
-                        <ListItem key={"item" + i}>
-                            <ListItemIcon>
-                                <Skeleton variant="circle" width={30} height={30}/>
-                            </ListItemIcon>
-                            <Skeleton variant="text" width={200} height={30}/>
-                        </ListItem>
-                    ))}
-                </ul>
-            </li>
-            )
-        )
+        <List className={classes.list}>
+            {[...new Array(5)].map((item, index) => (
+                    <li key={index} className={classes.listSection}>
+                        <ul className={classes.ul}>
+                            <ListSubheader>
+                                <Skeleton variant="text" width={70} height={20}/>
+                            </ListSubheader>
+                            {[...new Array(4)].map((item, i) => (
+                                <ListItem key={"item" + i}>
+                                    <ListItemIcon>
+                                        <Skeleton variant="circle" width={30} height={30}/>
+                                    </ListItemIcon>
+                                    <Skeleton variant="text" width={200} height={30}/>
+                                </ListItem>
+                            ))}
+                        </ul>
+                    </li>
+                )
+            )}
+        </List>
+
     )
 }
